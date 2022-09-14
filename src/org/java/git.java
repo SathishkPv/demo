97846823b08f8292b573c706157fcf6552a6ca31
SathishkPv <pvsathish1996@gmail.com>
2022-09-14 19:15:40 +0530

    done

---
+++ src/org/java/git.java
@@ -0,0 +1,16 @@
+package org.java;
+
+public class git {
+
+	private void code1() {
+		System.out.println("git hub testing");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		git g=new git();
+	    g.code1();
+
+	}
+
+}
